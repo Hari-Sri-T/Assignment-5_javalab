340a105cf2d8144a92c5f3249ed7f53906761029
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-02-12 11:59:31 +0530

    Added Rectangle.java
    with methods to calculate the Area and perimeter for Rectangle

--- Main.java
+++ Main.java
@@ -15,5 +15,19 @@ class Main{
 		System.out.println("Area of Triangle is: "+ traingle.calculateArea());
 		System.out.println("Perimeter of Triangle is: "+ traingle.calculatePerimeter());
 		System.out.println("___________________________________________");		
+		
+		System.out.println("Enter length of Rectangle: ");
+		double length = Double.parseDouble(scan.nextLine());
+		
+		System.out.println("Enter breadth of Rectangle: ");
+		double breadth = Double.parseDouble(scan.nextLine());
+		
+		System.out.println("___________________________________________");	
+		Shape rectangle = new Rectangle(length, breadth);
+		
+		System.out.println("Area of Triangle is: "+ rectangle.calculateArea());
+		System.out.println("Perimeter of Triangle is: "+ rectangle.calculatePerimeter());
+		System.out.println("___________________________________________");		
 	}
+	
 }
---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+// Rectangle.java
+
+import java.lang.*;
+class Rectangle extends Shape{
+	public Rectangle(double length,double breadth){
+		super(length, breadth,0,4);	
+	}
+	
+	public double calculateArea(){
+		return dim_one * dim_two;
+	}
+	
+	public double calculatePerimeter(){
+		return 2 * (dim_one + dim_two);
+	}
+}
