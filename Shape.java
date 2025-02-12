c257d4c34fe0fade0c84eceb78ae6c738f2a3492
Hari-Sri-T <harisrishanmukhasai2005@gmail.com>
2025-02-12 11:21:52 +0530

    Created Shape Abstract Class
    with Zero parameter, paraameterised constructors
    and 2 abstract classes calculateArea and calculatePerimeter along with a
    getNumSides to get the num of sides.

---
+++ Shape.java
@@ -0,0 +1,30 @@
+// Shape.java
+
+abstract class Shape{
+	protected double dim_one;
+	protected double dim_two;
+	protected double dim_three;
+	protected int num_sides;
+	
+	// Zero Parameters
+	public Shape(){
+		dim_one = dim_two = dim_three = 0;
+		num_sides = 0;
+	}
+	//Parameterized Constructor
+	public Shape(double dim_one, double dim_two, double dim_three, int num_sides){
+		this.dim_one = dim_one;
+		this.dim_two = dim_two;
+		this.dim_three = dim_three;
+		this num_sides = num_sides;
+	}
+	
+	abstract public void calculateArea(); //Abstract Methods
+	abstract public void calculatePerimeter();
+	
+	public int getNumSides(){
+		return num_sides;
+	}
+}
+
+
